010e7b9c0cfb0540aade5549ad2522a0c3e24678
jesper hellqvist <jh225ci@student.lnu.se>
2024-02-20 18:56:56 +0100

    slut för idag pos

--- src/js/BarometerContainer.js
+++ src/js/BarometerContainer.js
@@ -7,7 +7,7 @@ class BarometerContainer {
         this.barometerContainer.id = 'barometer-container';
         this.barometer = new Barometer(this.barometerContainer);
         this.background = document.createElement('img');
-        this.background.src = './src/js/img/barometer.png';
+        this.background.src = './src/js/img/barometer2.png';
         this.background.id = 'barometer-container-background';
         this.barometerContainer.appendChild(this.background);
         
