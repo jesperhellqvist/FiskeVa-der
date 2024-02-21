65e718fbf476451ff4a4c555d85d0674c08120a1
jesper hellqvist <jh225ci@student.lnu.se>
2024-02-21 16:24:41 +0100

    hevy

--- src/js/WeatherContainer.js
+++ src/js/WeatherContainer.js
@@ -12,14 +12,14 @@ class WeatherContainer {
         this.weatherImg = document.createElement('img');
         this.weatherImg.className = 'weather-img';
         this.weatherContainer.appendChild(this.weatherImg);
-       
+
     }
 
     update(temp, wind, windDirection) {
-        console.log( temp, wind, windDirection);
+        console.log(temp, wind, windDirection);
         this.tempContainer.update(temp);
-     this.windContainer.update(wind, windDirection);
-     
+        this.windContainer.update(wind, windDirection);
+
     }
 
     updateCity(city) {
@@ -27,43 +27,48 @@ class WeatherContainer {
         this.userPosition.update(city);
     }
 
-    setBackGround(weatherCode){
+    setBackGround(weatherCode) {
         console.log(weatherCode);
-        weatherCode = 2;
-       switch(weatherCode){
-        
-           case 0:
-               this.weatherContainer.style.background = "url('./src/js/weather/clearSky_0.png')";
-               break;
-           case 1:
-               this.weatherImg.src = './src/js/weather/cloud.png';
-               break;
-           case 2:
-            this.weatherImg.src =  './src/js/weather/regn.gif';
-            this.weatherAppContainer.style.backgroundColor = "#e0dfdf";
-               break;
-        //    case 3:
-        //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/brokenclouds.jpg')";
-        //        break;
-        //    case 4:
-        //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/shower.jpg')";
-        //        break;
-        //    case 5:
-        //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/rain.jpg')";
-        //        break;
-        //    case 6:
-        //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/thunderstorm.jpg')";
-        //        break;
-        //    case 7:
-        //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/snow.jpg')";
-        //        break;
-        //    case 8:
-        //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/mist.jpg')";
-        //        break;
-           default:
-               this.weatherContainer.style.backgroundImage = "url('./src/js/weather/clearSky_0.png')";
-       }
+        weatherCode = 63;
+        switch (weatherCode) {
+
+            case 0:
+                this.weatherContainer.style.background = "url('./src/js/weather/clearSky_0.png')";
+                break;
+            case 1:
+                this.weatherImg.src = './src/js/weather/cloud.png';
+                break;
+
+            case 53:
+            case 63:
+            case 81:
+                this.weatherImg.src = './src/js/weather/moderaterain.gif';
+
+                break;
+            case 55:
+            case 65:
+            case 82:
+                this.weatherImg.src = './src/js/weather/heavyrain.gif';
+                break;
+            //    case 4:
+            //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/shower.jpg')";
+            //        break;
+            //    case 5:
+            //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/rain.jpg')";
+            //        break;
+            //    case 6:
+            //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/thunderstorm.jpg')";
+            //        break;
+            //    case 7:
+            //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/snow.jpg')";
+            //        break;
+            //    case 8:
+            //        this.weatherContainer.style.backgroundImage = "url('./src/js/img/mist.jpg')";
+            //        break;
+            default:
+                this.weatherContainer.style.backgroundImage = "url('./src/js/weather/clearSky_0.png')";
+        }
     }
 
-  
+
 }
