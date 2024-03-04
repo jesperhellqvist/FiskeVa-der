10c69109f826ac54a41b2519732a5b0bcd29e80d
jesper hellqvist <jh225ci@student.lnu.se>
2024-03-04 11:30:25 +0100

    test

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,21 @@ const urlsToCache = [
     '/service-worker.js',
     '/src/js/Main.js',
     './src/js/css/style.css',
-    './src/js/img/icon_fiske.png'
+    './src/js/img/icon_fiske.png',
+    './src/js/WheatherApp.js',
+    './src/js/City.js',
+    './src/js/Weather.js',
+    './src/js/WeatherContainer.js',
+    './src/js/TempContainer.js',
+    './src/js/WindContainer.js',
+    './src/js/UserPositionContainer.js',
+    './src/js/HourlyWeather.js',
+    './src/js/BarometerContainer.js',
+    './src/js/FishAnimationContainer.js',
+    './src/js/UserPosition.js',
+    './src/js/img/barometer2.png',
+    './src/js/img/sjo3.png',
+    './src/js/img/windDir1.png',
 ];
 
 self.addEventListener('install', event => {
