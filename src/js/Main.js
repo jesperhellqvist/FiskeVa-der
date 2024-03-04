c4e1c18f6958be38474b4efe8269915676218150
jesper hellqvist <jh225ci@student.lnu.se>
2024-03-04 10:53:02 +0100

    test service-worker

--- src/js/Main.js
+++ src/js/Main.js
@@ -31,6 +31,20 @@ var Main = {
         }
     },
 
+    registerServiceWorker: function () {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/service-worker.js')
+                    .then(registration => {
+                        console.log('Service Worker registered with scope:', registration.scope);
+                    })
+                    .catch(error => {
+                        console.error('Service Worker registration failed:', error);
+                    });
+            });
+        }
+    }
+
 }
 
 window.addEventListener('load', Main.init);
