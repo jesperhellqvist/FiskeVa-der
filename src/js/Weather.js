b623bf7ac40852f1e363ef1eaa03a0fc947c9261
jesper hellqvist <jh225ci@student.lnu.se>
2024-03-05 18:01:46 +0100

    tt

--- src/js/Weather.js
+++ src/js/Weather.js
@@ -5,7 +5,7 @@ class Weather {
         this.lat = lat;
         this.lon = lon;
 
-        this.url_currentWeather = "https://api.open-meteo.com/v1/forecast?latitude="+ this.lat + "&longitude=" + this.lon + "&currrent=temperature_2m,is_day,weather_code,pressure_msl,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,pressure_msl,weather_code,wind_speed_10m,wind_direction_10m&daily=weather_code&wind_speed_unit=ms&timezone=Europe%2FBerlin&forecast_days=1";
+        this.url_currentWeather = "https://api.open-meteo.com/v1/forecast?latitude="+ this.lat + "&longitude=" + this.lon + "&current=temperature_2m,is_day,weather_code,pressure_msl,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,pressure_msl,weather_code,wind_speed_10m,wind_direction_10m&daily=weather_code&wind_speed_unit=ms&timezone=Europe%2FBerlin&forecast_days=1";
 
 
         this.currentWeather = {};
