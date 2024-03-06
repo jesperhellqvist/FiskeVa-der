aac37d2677edf8bdfeece6229048d238b7224b34
jesper hellqvist <jh225ci@student.lnu.se>
2024-03-06 17:10:35 +0100

    ny lista

--- service-worker.js
+++ service-worker.js
@@ -6,10 +6,23 @@ const urlsToCache = [
     '/manifest.json',
     '/service-worker.js',
     '../src/js/css/style.css',
+    '../src/js/Main.js',
+    '../src/js/WeatherApp.js',
+    '../src/js/Weather.js',
+    '../src/js/UserPosition.js',
+    '../src/js/City.js',
+    '../src/js/Barometer.js',
+    '../src/js/WeatherContainer.js',
+    '../src/js/TempContainer.js',
+    '../src/js/WindContainer.js',
+    '../src/js/UserPositionContainer.js',
+    '../src/js/HourlyWeather.js',
+    '../src/js/BarometerContainer.js',
+    '../src/js/FishAnimationContainer.js',
     '../src/js/img/barometer2.png',
     '../src/js/img/sjo3.png',
     '../src/js/img/windDir1.png',
-   
+    '../src/js/img/abborre.png',
     '../src/js/weather/clearSky.png',
     '../src/js/weather/heavyrain.gif',
     '../src/js/weather/lightrain.gif',
@@ -26,61 +39,50 @@ const urlsToCache = [
     '../src/js/icons/sno.png',
     '../src/js/icons/sol.png',
     '../src/js/icons/nastanKlart.png',
-    '../src/js/img/abborre.png',
-    '../src/js/WeatherApp.js',
-    '../src/js/Weather.js',
-    '../src/js/UserPosition.js',
-    '../src/js/City.js',
-    '../src/js/Barometer.js',
-    '../src/js/WeatherContainer.js',
-    '../src/js/TempContainer.js',
-    '../src/js/WindContainer.js',
-    '../src/js/UserPositionContainer.js',
-    '../src/js/HourlyWeather.js',
-    '../src/js/BarometerContainer.js',
-    '../src/js/FishAnimationContainer.js',
-    '../src/js/Main.js',
+
+
+
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(cache => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-        .then(response => {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-
-            // Clone the request because it's a stream
-            let fetchRequest = event.request.clone();
-
-            return fetch(fetchRequest)
-                .then(response => {
-                    // Check if we received a valid response
-                    if (!response || response.status !== 200 || response.type !== 'basic') {
-                        return response;
-                    }
-
-                    // Clone the response because it's a stream
-                    let responseToCache = response.clone();
-
-                    caches.open(CACHE_NAME)
-                        .then(cache => {
-                            cache.put(event.request, responseToCache);
-                        });
-
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
                     return response;
-                });
-        })
+                }
+
+                // Clone the request because it's a stream
+                let fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest)
+                    .then(response => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response because it's a stream
+                        let responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
+            })
     );
 });
