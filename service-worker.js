dd88b4a30e891913997221ea7af947596f82d4cf
jesper hellqvist <jh225ci@student.lnu.se>
2024-03-06 16:07:54 +0100

    test servi

--- service-worker.js
+++ service-worker.js
@@ -38,12 +38,20 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            if (response) {
-                return response;
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
             }
-            return fetch(event.request);
+
+            return fetch(event.request).then(response => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        }).catch(() => {
+            var errorScreen = document.getElementById('errorScreen');
+            errorScreen.style.display = 'flex';
         })
     );
 });
