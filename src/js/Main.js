2479fad2caf95a3f8011fbe9820e571d37bbec2e
jesper hellqvist <jh225ci@student.lnu.se>
2024-03-14 16:37:05 +0100

    test

--- src/js/Main.js
+++ src/js/Main.js
@@ -35,7 +35,7 @@ var Main = {
     registerServiceWorker: function () {
         
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('./../../service-worker.js')
+            navigator.serviceWorker.register('service-worker.js')
             .then(registration => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
