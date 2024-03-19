93f083dec81fce81985d21a892c9a36fe8404261
jesper hellqvist <jh225ci@student.lnu.se>
2024-03-19 09:17:20 +0100

    test igen

--- src/js/Main.js
+++ src/js/Main.js
@@ -40,42 +40,55 @@ var Main = {
     },
 
     pullToRefresh: function () {
+        this.window.addEventListener('touchstart', (event) => {
+            var startY = event.touches[0].clientY;
+        }, false);
 
-        window.addEventListener('touchstart', Main.handleTouchStart.bind(this), false);
-        window.addEventListener('touchmove', Main.handleTouchMove.bind(this), false);
-        Main.xDown = null;
-        Main.yDown = null;
-
+        this.window.addEventListener('touchmove', (event) => {
+            var endY = event.touches[0].clientY;
+            if (endY > startY) {
+                console.log('pull down');
+                Main.weatherApp.refreshWeatherData();
+            }
+        }, false);
     },
+    // pullToRefresh: function () {
 
-    handleTouchStart: function (evt) {
-        console.log('touchstart');
-        Main.xDown = evt.touches[0].clientX;
-        Main.yDown = evt.touches[0].clientY;
-    },
+    //     window.addEventListener('touchstart', Main.handleTouchStart.bind(this), false);
+    //     window.addEventListener('touchmove', Main.handleTouchMove.bind(this), false);
+    //     Main.xDown = null;
+    //     Main.yDown = null;
 
-    handleTouchMove: function (evt) {
-        console.log('touchmove');
-        if (!Main.xDown || !Main.yDown) {
-            console.log('return');
-            return;
-        }
+    // },
 
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
+    // handleTouchStart: function (evt) {
+    //     console.log('touchstart');
+    //     Main.xDown = evt.touches[0].clientX;
+    //     Main.yDown = evt.touches[0].clientY;
+    // },
 
-        var xDiff = Main.xDown - xUp;
-        var yDiff = Main.yDown - yUp;
-        if ( Math.abs( xDiff ) < Math.abs( yDiff ) && yDiff < 0 ) {
-            console.log('pull down');
-            /* reset values */
-            Main.xDown = null;
-            Main.yDown = null;
-            // Refresh the weather data
-            Main.weatherApp.refreshWeatherData();
-            evt.preventDefault();
-        }  
-    },
+    // handleTouchMove: function (evt) {
+    //     console.log('touchmove');
+    //     if (!Main.xDown || !Main.yDown) {
+    //         console.log('return');
+    //         return;
+    //     }
+
+    //     var xUp = evt.touches[0].clientX;
+    //     var yUp = evt.touches[0].clientY;
+
+    //     var xDiff = Main.xDown - xUp;
+    //     var yDiff = Main.yDown - yUp;
+    //     if ( Math.abs( xDiff ) < Math.abs( yDiff ) && yDiff < 0 ) {
+    //         console.log('pull down');
+    //         /* reset values */
+    //         Main.xDown = null;
+    //         Main.yDown = null;
+    //         // Refresh the weather data
+    //         Main.weatherApp.refreshWeatherData();
+    //         evt.preventDefault();
+    //     }  
+    // },
 
     /**
        * Registrerar service worker.
